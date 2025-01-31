25f2ec53c9df8cbcfeb23fcd05fe551280e3da0d
Nahome18 <101459741+Nahome18@users.noreply.github.com>
2025-01-31 03:14:28 -0600

    made visa component reusable, implemented phones pages

--- client/src/components/Visa.jsx
+++ client/src/components/Visa.jsx
@@ -1,15 +1,7 @@
 import '../styles/Visa.css'
 
-export default function Visa(){
-    const photo = "VISA CARD PIC"
-    const sentence = "Be it for convenience or for privacy, the MELD debit card is a powerful tool."
-    const para = "For those who want to spend and not worry about being blocked or frozen out of your account."
-    const info1_t = "16 Currencies"
-    const info2_t = "Low FX Fees"
-    const info1 = "Send and receive EUR, USD, GBP, JPY, CHF, AUD, SDG, CAD, SEK, DKK, PLN, RON, HUF, HKD and AED."
-    const info2 = "Convert between different fiat currencies and between fiat and crypto for as low as 0.5%."
-    const info1Pic = "Cash Pic"
-    const info2Pic = "Coint Pic"
+export default function Visa({photo, sentence, para, info1_t, info2_t, info1, info2, info1Pic, info2Pic}){
+
     return (
         <div className='visa'>
             <div className='pic'>
