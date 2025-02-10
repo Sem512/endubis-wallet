23a7536a9144a3bb3d2d71e622d2b35641318571
Semir Sultan Mohammed <semi512113@gmail.com>
2025-02-11 01:30:37 +0300

    visual bug fixes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,39 @@
-import '../styles/header.css'
+import React, { useState } from 'react';
+import '../styles/header.css';
 
-export default function Header(){
-    return(
-        <header>
-            <div id="logo">
-               <img src={require('../images/Endubis - Horizontal Logo - White.png')} alt="fd" />
-            </div>
-        
-        <ul>
-        <li><a href="crash"><span className='link'>FIAT</span></a></li>
-        <li><a href="crash"><span className='link'>CRYPTO</span></a></li>
-        </ul>
-        </header>
-    )
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header>
+      <div id="logo">
+        <img src={require('../images/Endubis - Horizontal Logo - White.png')} alt="fd" />
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
+      {/* Navigation Menu */}
+      <ul className={isMenuOpen ? 'open' : ''}>
+        <li>
+          <a href="crash">
+            <span className="link">FIAT</span>
+          </a>
+        </li>
+        <li>
+          <a href="crash">
+            <span className="link">CRYPTO</span>
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
 }
